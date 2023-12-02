0f2fb0309a51c5cb5c96393a7f4b2483e7eeb72e
Goutham Kurapati <gouthamkurapati@gmail.com>
2023-12-02 14:34:38 +0530

    Added FCFS CPU Scheduling

---
+++ CPU Scheduling Algorithms/FCFS.py
@@ -0,0 +1,36 @@
+#Read the inputs from user
+PROCESSES = []
+n = int(input("Enter no. of processes : "))
+for i in range(n):
+    name = input("Enter process name : ")
+    at = int(input("Enter process arrival time : "))
+    bt = int(input("Enter process burst time : "))
+    PROCESSES.append({"name":name, "at":at, "bt":bt})
+
+#Apply the algorithm
+processes = list(PROCESSES)
+processes.sort(key=lambda P: P['at'])
+curr_time = 0
+for process in processes :
+    if (process["at"]>curr_time):
+        curr_time = process["at"]
+    process["rt"] = curr_time-process["at"]
+    curr_time += process["bt"]
+    process["ct"] = curr_time
+    process["tat"] = process["ct"]-process["at"]
+    process["wt"] = process["tat"]-process["bt"]
+
+#Display the results
+print("--- FCFS CPU SCHEDULING ALGORITHM ---")
+print("\n--- PROCESS TABLE ---")
+print("P", "AT", "BT", "CT", "TAT", "WT", "RT", sep="\t")
+t_tat = t_wt = t_rt = 0
+for p in processes:
+    print(p["name"], p["at"], p["bt"], p["ct"], p["tat"], p["wt"], p["rt"], sep="\t")
+    t_tat += p["tat"]
+    t_wt += p["wt"]
+    t_rt += p["rt"]
+print("\n")
+print("Average TAT =", round(t_tat/n,2))
+print("Average WT =", round(t_wt/n,2))
+print("Average RT =", round(t_rt/n,2))
